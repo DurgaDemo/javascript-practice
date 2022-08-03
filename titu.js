ee4e29bba43469106d6056393b1a7781e9220bc4
Durga <durgamohapatra303@gmail.com>
2022-08-03 16:38:55 +0530

    All javascript concept

---
+++ titu.js
@@ -0,0 +1,16 @@
+//  function getSum(n1, n2, n3) {
+//     return n1 + n2 + n3;
+// }
+// function getSum(n1, n2) {
+//     return n1 + n2;
+// }
+// var sum = getSum(2, 3, 10);
+// console.log(sum); // 5
+
+
+// let a="i am durga";
+// let b=a.split('').reverse().join('');
+// console.log(b);
+
+
+
